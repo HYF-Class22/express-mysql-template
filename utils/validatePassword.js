3c86fb464a43fe8454fbbfb0a113b438329d403d
samirm00 <sammoham.be@gmail.com>
2024-07-03 12:04:48 +0200

    Update password

--- utils/validatePassword.js
+++ utils/validatePassword.js
@@ -1,5 +1,15 @@
+/*
+    This function is used to validate the password.
+    - The password must be between 6 to 16 characters long.
+    - The password must contain at least one numeric digit.
+    - The password must contain at least one uppercase letter.
+    - The password must contain at least one special character.
+    - The password must not contain any whitespace.
+*/
+
 const validatePassword = (password) => {
-    const pattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    const pattern =
+        /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{6,16}$/;
     if (pattern.test(password)) {
         return true;
     } else {
